e0d4818df2e5b837eee0099d34d47b19adf188e7
Minh Quân Lê <minhquanle@gmail.com>
2023-03-24 16:58:11 +0700

    feat: authentication, authorization and security

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
@@ -15,6 +16,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
@@ -33,6 +39,13 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 })
 
 userSchema.pre('save', async function (next) {
@@ -42,6 +55,19 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, 12)
 
   this.passwordConfirm = undefined
+  next()
+})
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next()
+
+  this.passwordChangedAt = Date.now() - 1000
+  next()
+})
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } })
+
   next()
 })
 
@@ -62,6 +88,20 @@ userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
   }
 
   return false
+}
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex')
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+
+  // Send this token to user mail that forgot password then parse it to reset token with createHash in resetPassword api to compare with passwordResetToken in DB
+  return resetToken
 }
 
 const User = mongoose.model('User', userSchema)
